6d001f537cf2184a442d63c5abfb0b3954ebb5a6
aherman21 <hermansalexander2@gmail.com>
2023-11-02 16:13:01 +0100

    App.js and MainMenu.js

---
+++ src/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerContainer: {
+        flex: 0.5,
+        backgroundColor: '#fff',
+        alignItems: 'top',
+        justifyContent: 'top',
+      },
+    headers: {
+        fontSize: 30,
+        color: 'Black',
+    },
+  });
