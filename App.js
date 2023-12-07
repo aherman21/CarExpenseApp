ea50cd57e1393f5e55154d42b0ed868951b27e79
aherman21 <hermansalexander2@gmail.com>
2023-12-07 16:33:45 +0100

    changed file names to be consistent

--- App.js
+++ App.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import TripsScreen from './src/screens/TripsScreen2'; // Adjusted import path
+import TripsScreen from './src/screens/TripsScreen'; // Adjusted import path
 import MainScreen from './src/screens/MainScreen';
 import ShowTrip from './src/components/ShowTrip';
 import { Button } from 'react-native';
